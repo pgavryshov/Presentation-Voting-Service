3619a0aacafaf2a075f34c58c84028e67606d28f
pgavryshov <pgavryshov@lineate.com>
2019-02-07 15:49:44 +0400

    initial add MAVEN and Spring Boot 2

---
+++ src/main/java/com/thumbtack/presentation_vouting_service/PVSApplication.java
@@ -0,0 +1,13 @@
+package com.thumbtack.presentation_vouting_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PVSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PVSApplication.class, args);
+        System.out.println("Start");
+    }
+}
---
+++ src/test/java/com/thumbtack/presentation_vouting_service/PVSApplicationTest.java
@@ -0,0 +1,19 @@
+package com.thumbtack.presentation_vouting_service;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest()
+@RunWith(SpringRunner.class)
+public class PVSApplicationTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
